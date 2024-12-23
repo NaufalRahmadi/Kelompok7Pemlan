dac1db126ca5afab1614bd3350e21a7f4a9b1ce7
achmadsalmanpasha <87318407+achmadsalmanpasha@users.noreply.github.com>
2024-12-23 18:27:40 +0700

    Add files via upload
    Signed-off-by: achmadsalmanpasha <87318407+achmadsalmanpasha@users.noreply.github.com>

---
+++ sortir_barang.c
@@ -0,0 +1,35 @@
+﻿void sortirBarang() {
+    if (jumlahBarang == 0) {
+        printf("Inventaris kosong.\n");
+        return;
+    }
+
+    int pilihan;
+    printf("\nPilih kriteria sortir:\n");
+    printf("1. Berdasarkan ID\n");
+    printf("2. Berdasarkan Nama\n");
+    printf("3. Berdasarkan Waktu (Tanggal barang ditambahkan)\n");
+    printf("Pilih opsi: ");
+    scanf("%d", &pilihan);
+
+    int i, j;
+    for (i = 0; i < jumlahBarang - 1; i++) {
+        for (j = i + 1; j < jumlahBarang; j++) {
+            int swap = 0;
+            if (pilihan == 1 && barang[i].id > barang[j].id) {
+                swap = 1;
+            } else if (pilihan == 2 && strcmp(barang[i].nama, barang[j].nama) > 0) {
+                swap = 1;
+            } else if (pilihan == 3 && strcmp(barang[i].tanggal, barang[j].tanggal) > 0) {
+                swap = 1;
+            }
+            if (swap) {
+                Barang temp = barang[i];
+                barang[i] = barang[j];
+                barang[j] = temp;
+            }
+        }
+    }
+
+    printf("Barang berhasil diurutkan.\n");
+}
