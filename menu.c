2c954613e5dcc4425b1c6badba35a977c85575a9
Novian Zulfi Mujtaba <143938935+Novianzulfi@users.noreply.github.com>
2024-12-23 19:58:41 +0700

    Update menu.c
    Signed-off-by: Novian Zulfi Mujtaba <143938935+Novianzulfi@users.noreply.github.com>

--- menu.c
+++ menu.c
@@ -1,11 +1,33 @@
-// Fungsi untuk menampilkan menu
-void tampilkanMenu() {
-    printf("\n=== Program Manajemen Inventaris ===\n");
-    printf("1. Tambah Barang\n");
-    printf("2. Tampilkan Daftar Barang\n");
-    printf("3. Cari Barang\n");
-    printf("4. Hapus Barang\n");
-    printf("5. Sortir Barang\n");
-    printf("6. Keluar\n");
-    printf("Pilih opsi: ");
+void menuBarang() {
+    int pilihan;
+    do {
+        printf("\nMenu Barang:\n");
+        printf("1. Tambah Barang\n");
+        printf("2. Hapus Barang\n");
+        printf("3. Cari Barang\n");
+        printf("4. Lihat Barang\n");
+        printf("5. Kembali ke Menu Utama\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+        case 1:
+            insertBarang();
+            break;
+        case 2:
+            deleteBarang();
+            break;
+        case 3:
+            searchBarang();
+            break;
+        case 4:
+            viewBarang();
+            break;
+        case 5:
+            printf("Kembali ke Menu Utama.\n");
+            break;
+        default:
+            printf("Pilihan tidak valid!\n");
+        }
+    } while (pilihan != 5);
 }
