4507fb1a0717bd365eb39043af4e4186fd9fb343
Novian Zulfi Mujtaba <143938935+Novianzulfi@users.noreply.github.com>
2024-12-23 19:17:12 +0700

    Update menu.c
    Signed-off-by: Novian Zulfi Mujtaba <143938935+Novianzulfi@users.noreply.github.com>

---
+++ menu.c
@@ -0,0 +1,11 @@
+// Fungsi untuk menampilkan menu
+void tampilkanMenu() {
+    printf("\n=== Program Manajemen Inventaris ===\n");
+    printf("1. Tambah Barang\n");
+    printf("2. Tampilkan Daftar Barang\n");
+    printf("3. Cari Barang\n");
+    printf("4. Hapus Barang\n");
+    printf("5. Sortir Barang\n");
+    printf("6. Keluar\n");
+    printf("Pilih opsi: ");
+}
